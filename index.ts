f53c0f3e8f6968ef530aba3226ae96857bc2f89e
Bynio <=>
2022-02-26 15:28:26 +0100

    Adding modules to package and check if express is working properly

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World")
+import * as express from 'express';
+
+const app = express();
+
+app.get('/' , (req, res) => {
+    res.send('Hello World')
+})
+
+//Over 9000
+app.listen(9001, 'localhost' , () => {
+    console.log("I'm listening on http://localhost:9001")
+})
